f72e7cb3c80c45dd725bde82c7394509236fa1fc
Hari Ramasamy <hari.csesoft@gmail.com>
2018-03-14 23:28:20 +0530

    refs #Modified_commit.

--- PrintF.c
+++ PrintF.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main(){
+
  /*   1. Write a C program to print your name, date of birth. and mobile number. Go to the editor
 Expected Output:
 
 Name   : Alexandra Abramov
 DOB    : July 14, 1975
 Mobile : 99-9999999999*/
+
 char name[30],dob[20],mobile[20];
 printf("\nEnter Your Name : ");
 gets(name);
